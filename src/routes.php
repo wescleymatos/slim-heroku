a53605631b5a97deccc417f67e9387f5ae1fa3fb
Wescley Matos <wescleymatos@gmail.com>
2017-03-08 00:11:32 -0300

    nova rota

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,18 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/clientes', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'clientes.phtml', $args);
 });
